3d49ff11aca0a1dd50baa997ac48797ec2d30960
[200~anusha355~ <amulya.sarimalla@gmail.com>
2024-08-01 10:01:22 +0530

    first

---
+++ vowel.java
@@ -0,0 +1,37 @@
+class Three
+{
+	public static void main(String args[])
+{
+	char s='a';
+	if(s==0 && s==9)
+{
+
+	System.out.println("vowel");
+}
+	else
+{
+	System.out.println("consonant");
+}
+	switch(s){
+	case 'a':
+	  System.out.println("apple");
+	break;
+	case 'e':
+	  System.out.println("elephant");
+	break;
+	case 'i':
+	  System.out.println("ice");
+	break;
+	case 'o':
+	  System.out.println("oxe");
+	break;
+	case 'u':
+	  System.out.println("vowel");
+      	break;
+	default:
+	  System.out.println("consonant");
+	
+
+}
+}
+}
